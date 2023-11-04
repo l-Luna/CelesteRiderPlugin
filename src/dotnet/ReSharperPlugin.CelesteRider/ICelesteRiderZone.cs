bcd502df51557e9cb0e8758959724ad9246a3ac5
l-Luna <izzeldeen03@gmail.com>
2023-11-04 00:45:13 +0000

    Initial commit

---
+++ src/dotnet/ReSharperPlugin.CelesteRider/ICelesteRiderZone.cs
@@ -0,0 +1,7 @@
+using JetBrains.Application.BuildScript.Application.Zones;
+
+namespace ReSharperPlugin.CelesteRider;
+
+[ZoneDefinition]
+// [ZoneDefinitionConfigurableFeature("Title", "Description", IsInProductSection: false)]
+public interface ICelesteRiderZone : IZone{}
